4e53515a5ab8dc425edaed5bbd08fe1aaccc5e39
chenlixue <lixue.chen@monstar-lab-cn.com>
2017-11-13 18:28:45 +0800

    fix layout

---
+++ application/config/menu.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: monstar
+ * Date: 2017/11/13
+ * Time: 下午4:48
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['asideMenu'] = array(
+    array(
+        'menu' => 'Menu0',
+        'icon' => 'dashboard',
+        'path' => '',
+        'subMenu' => array()
+    ),
+    array(
+        'menu' => 'Menu1',
+        'icon' => 'dashboard',
+        'path' => '',
+        'subMenu' => array(
+            array(
+                'menu' => 'Menu1-1',
+                'icon' => '',
+                'path' => '',
+            ),
+            array(
+                'menu' => 'Menu1-2',
+                'icon' => '',
+                'path' => '',
+            )
+        )
+    ),
+    array(
+        'menu' => 'Menu2',
+        'icon' => 'dashboard',
+        'path' => '',
+        'subMenu' => array(
+            array(
+                'menu' => 'Menu2-1',
+                'icon' => '',
+                'path' => '',
+            ),
+            array(
+                'menu' => 'Menu2-2',
+                'icon' => '',
+                'path' => '',
+            )
+        )
+    ),
+    array(
+        'menu' => 'Menu3',
+        'icon' => 'dashboard',
+        'path' => '',
+        'subMenu' => array(
+            array(
+                'menu' => 'Menu3-1',
+                'icon' => '',
+                'path' => '',
+            ),
+            array(
+                'menu' => 'Menu3-2',
+                'icon' => '',
+                'path' => '',
+            )
+        )
+    )
+);
--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -8,9 +8,22 @@
 
 class Test extends MY_Controller
 {
+    public function __construct(){
+        parent::__construct();
+    }
+
     public function index(){
-        $this->assign('describe', '这个控制器及其对应的页面均为系统测试页面！！！');
-        $this->assign('testKey001', 'testVal001');
-        $this->display(APPPATH . 'views/templates/test.html');
+        $layout = [];
+        $layout['page'] = 'test';
+        $layout['title'] = 'Page1';
+        $layout['asideMenu'] = config_item('asideMenu');
+        $this->assign('layout', $layout);
+
+        $data = [];
+        $data['describe'] = '这个控制器及其对应的页面均为系统测试页面！！！';
+        $data['testKey001'] = 'testVal001';
+        $this->assign('data', $data);
+
+        $this->display(APPPATH . 'views/templates/admin/layout.html');
     }
 }
