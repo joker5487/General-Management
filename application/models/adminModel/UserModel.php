74fca2135670eb17cbe8b6a30a4f9b87d3d5113a
chenlixue <lixue.chen@monstar-lab-cn.com>
2017-11-22 18:22:44 +0800

    fix user model

--- application/models/adminModel/UserModel.php
+++ application/models/adminModel/UserModel.php
@@ -15,4 +15,22 @@ class UserModel extends MY_Model
 
         return $userList;
     }
+
+    public function insert_user($userInfo){
+        $res = $this->db->insert('user_admin', $userInfo);
+
+        return $res;
+    }
+
+    public function update_user($userInfo, $userId){
+        $res = $this->db->update('user_admin', $userInfo, ['id' => $userId]);
+
+        return $res;
+    }
+
+    public function delete_user($userId){
+        $res = $this->db->delete('user_admin', ['id' => $userId]);
+
+        return $res;
+    }
 }
