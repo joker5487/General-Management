16228d0c1d00a1c9ed43d15db7242e1a4516fa9a
chenlixue <lixue.chen@monstar-lab-cn.com>
2018-05-28 18:13:57 +0800

    修改文件处理类

--- application/libraries/Document.php
+++ application/libraries/Document.php
@@ -10,10 +10,16 @@ class Document
 {
     private $_dir;
 
+    // 初始化默认路径
     public function __construct(){
-        $this->_dir = FCPATH . 'application/logs/logtest/';
+        $this->_dir = FCPATH . 'application/logs/';
     }
 
+    /* 获取指定目录下面的文件夹和文件
+     * @param string $path 指定的文件夹目录
+     * @param string $sort 文件排序标识
+     * @param string $filed 文件排序信息标识(目前只有 name - 文件名称 filectime - 创建时间 filemtime - 修改时间)
+     * */
     public function get_documents($path = '', $sort = 'desc', $filed = 'filemtime'){
         $folders = [];
         $files = [];
@@ -21,6 +27,7 @@ class Document
         $dir = $this->_dir . $path;
         $sort = strtolower($sort);
 
+        // 如果给定的路径是文件夹，则获取该路径下的所有文件夹和文件(获取名称、创建时间、修改时间)
         if(is_dir($dir)){
             if($dh = opendir($dir)){
                 while(($file = readdir($dh)) !== FALSE){
@@ -46,6 +53,7 @@ class Document
             }
         }
 
+        // 对该路径下面的文件进行分类排序
         if($sort === 'desc'){
             $sortOder = SORT_DESC;
         }else{
@@ -58,16 +66,20 @@ class Document
         $documents['folders'] = $folders;
         $documents['files'] = $files;
 
-        echo '<pre>';
-        var_dump($documents);
         return $documents;
     }
 
-    function my_sort($arrays, $sort_key, $sort_order = SORT_DESC, $sort_type = SORT_REGULAR){
+    /* 二维数组排序(此方法可以提成一个公共的helper)
+     * @param array $arrays 需要进行排序的二维数组
+     * @param string $sortKey 排序字段
+     * @param string $sortOrder 排序标识
+     * @param string $sortType 排序类型标识
+     * */
+    function my_sort($arrays, $sortKey, $sortOrder = SORT_DESC, $sortType = SORT_REGULAR){
         if(is_array($arrays)){
             foreach ($arrays as $array){
                 if(is_array($array)){
-                    $key_arrays[] = $array[$sort_key];
+                    $keyArrays[] = $array[$sortKey];
                 }else{
                     return false;
                 }
@@ -75,7 +87,7 @@ class Document
         }else{
             return false;
         }
-        array_multisort($key_arrays,$sort_order,$arrays);
+        array_multisort($keyArrays, $sortOrder, $sortType, $arrays);
         return $arrays;
     }
 }
